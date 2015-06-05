1ab589f7ea3f488f51e2710ff8c403b40a4f20f1
elnon <elenenono@gmail.com>
2015-06-05 14:18:42 +0200

    #1 first try first fruit can be split

--- FruitNinja/src/de/duererInfoProject/fruitNinja/Universe.java
+++ FruitNinja/src/de/duererInfoProject/fruitNinja/Universe.java
@@ -5,6 +5,7 @@ import java.awt.Graphics2D;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
@@ -27,12 +28,33 @@ public class Universe extends JPanelBG {
 		lblNewLabel.setIcon(new ImageIcon(Universe.class.getResource("/de/duererInfoProject/fruitNinja/img/pause-icon.png")));
 		lblNewLabel.setBounds(10, 11, 48, 48);
 		add(lblNewLabel);
+		
+		
+		
+		JLabel pmgrn = new JLabel("");
+		Icon pmgrnNormal = new ImageIcon(Universe.class.getResource("/de/duererInfoProject/fruitNinja/img/Pomegranate.png"));
+		Icon pmgrnSplit = new ImageIcon(Universe.class.getResource("/de/duererInfoProject/fruitNinja/img/PomegranateSplit.png"));
+		pmgrn.setIcon(pmgrnNormal);
+		
+		pmgrn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				pmgrn.setIcon(pmgrnSplit);
+			}
+		});
+		
+		pmgrn.setBounds(50, 50, 200, 200);
+		add(pmgrn);
+
+		
 	}
 	
 	//Override paint Method to also paint the game
 	public void paint(Graphics g) {
 		super.paint(g);
 		Graphics2D g2d = (Graphics2D) g;
+//		img = ImageIO.read(new File("Pomegrenate.png"));
+//		g2d.drawString("Hello", 10, 10);
 		//Paint parts
 	}
 }
